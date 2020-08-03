4f1e5bc43da06176708031ab356c77fb92a87e6a
sebasegura97 <sebasegura97@gmail.com>
2020-08-03 12:15:28 -0300

    add pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  apps: [
+    {
+      name: "pcus-server",
+      script: "dist/index.js",
+      watch: true,
+      env: {
+        NODE_ENV: "development",
+      },
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: "SSH_USERNAME",
+      host: "SSH_HOSTMACHINE",
+      ref: "origin/master",
+      repo: "GIT_REPOSITORY",
+      path: "DESTINATION_PATH",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "npm install && pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+    },
+  },
+};
